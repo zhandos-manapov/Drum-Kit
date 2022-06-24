26e4c27b9889648807ac52612066180176af7275
zhandos-manapov <zandosmanapov@gmail.com>
2022-06-24 17:05:42 +0600

    some refactoring

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 var buttons = document.querySelectorAll("button");
 
 dict = {
-    "w":"tom-1.mp3", 
-    "a":"tom-2.mp3",
-    "s":"tom-3.mp3",
-    "d":"tom-4.mp3",
-    "j":"crash.mp3",
-    "k":"kick-bass.mp3",
-    "l":"snare.mp3"
+    "w": "tom-1.mp3",
+    "a": "tom-2.mp3",
+    "s": "tom-3.mp3",
+    "d": "tom-4.mp3",
+    "j": "crash.mp3",
+    "k": "kick-bass.mp3",
+    "l": "snare.mp3"
 }
 
 for (var button of buttons) {
     button.addEventListener("click", function () {
         var innerLetter = this.innerHTML;
-        var address = "sounds/" + dict[innerLetter];
-        var sound = new Audio(address);
-        sound.play();
+        makeSound(innerLetter);
     });
 }
 
-document.addEventListener("keydown", function (event){
+document.addEventListener("keydown", function (event) {
     var letter = event.key;
-    var address = "sounds/" + dict[letter];
+    makeSound(letter);
+})
+
+function makeSound(key){
+    var address = "sounds/" + dict[key];
     var sound = new Audio(address);
     sound.play();
 }
-)
 
 
